10aeeb709567fdc0ef8b23e72705543522d7c6ea
Nilesh <nileshabhang7875@gmail.com>
2022-02-05 20:48:38 +0530

    added binary search to project

---
+++ src/BinarySearch.java
@@ -0,0 +1,19 @@
+
+public class BinarySearch {
+	public static int binarySearch(int [] arr, int low, int high, int element) {
+		if(high >= low) {
+			int mid = low + (high - low)/2;
+			if(arr[mid] == element)
+				return mid;
+			if(arr[mid] > element)
+				return binarySearch(arr, low, mid -1 , element);
+			return binarySearch(arr, mid + 1, high, element);
+		}
+		return -1;
+	}
+	public static void main(String[] args) {
+		int arr[] = {1,2,3,4,5,6,7,8};
+		System.out.println("Index of 4: "+binarySearch(arr, 0, arr.length -1 , 6));
+
+	}
+}
